4614aba5a527f9acf9e2e8254fde4ae9046c07b4
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-30 13:35:06 +0000

    artwork migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_30_133143) do
+ActiveRecord::Schema.define(version: 2019_10_30_133407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artworks", force: :cascade do |t|
+    t.string "artist"
+    t.string "title"
+    t.string "medium"
+    t.text "description"
+    t.string "collection"
+    t.string "location"
+    t.string "image_url"
+    t.string "image_copyright"
+    t.string "permalink"
+    t.integer "origin_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "exhibition_likes", force: :cascade do |t|
     t.bigint "user_id", null: false
