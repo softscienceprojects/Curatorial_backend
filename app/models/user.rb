697d250ff110b7dfc55b928b874f38b7ea6c99fe
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-30 14:18:46 +0000

    following to symbol

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :relationships
 
     has_many :follower_relationships, foreign_key: :following_id, class_name: "Relationship"
-    has_many :followers, through: :follower_relationships, source: follower_relationships
+    has_many :followers, through: :follower_relationships, source: :follower_relationships
 
     has_many :following_relationships, foreign_key: :user_id, class_name: "Relationship"
-    has_many :following, through: :following_relationships, source: following
+    has_many :following, through: :following_relationships, source: :following
 
     has_many :exhibition_likes
     has_many :exhibitions
