b7275bc6a195dab09c04c347c2fd5399fc42dd91
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-30 13:28:49 +0000

    relationship resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+    has_many :relationships
+
+    has_many :follower_relationships, foreign_key: :following_id, class_name: "Relationship"
+    has_many :followers, through: :follower_relationships, source: follower_relationships
+
+    has_many :following_relationships, foreign_key: :user_id, class_name: "Relationship"
+    has_many :following, through: :following_relationships, source: following
+
+    has_many :exhibition_likes
+    has_many :exhibitions
+
+    has_many :reviews
+
 end
