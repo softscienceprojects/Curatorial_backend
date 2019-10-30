70fbc3b92341ed6206d44d52b2a1b49765653e7e
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-30 13:37:21 +0000

    content migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_30_133407) do
+ActiveRecord::Schema.define(version: 2019_10_30_133556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(version: 2019_10_30_133407) do
     t.string "image_copyright"
     t.string "permalink"
     t.integer "origin_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "contents", force: :cascade do |t|
+    t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
