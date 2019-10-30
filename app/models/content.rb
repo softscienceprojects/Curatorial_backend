a40ad5f4b12fb0dea3acf9f28a4c40bffebeef72
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-30 18:00:01 +0000

    works but ends undefined method ` ' for #<Class:0x00007fbca69c1138>)

--- app/models/content.rb
+++ app/models/content.rb
@@ -4,11 +4,9 @@ class Content < ApplicationRecord
     has_many :artworks, through: :artwork_contents
 
 
-    def self.make_content(data)
-   
-            
-            content = self.find_or_create_by(description: label.description.downcase)
-            ArtworkContent.create(content_id: content.id, artwork_id: id, score: label.score, confidence: label.confidence, topicality: label.topicality)
+    def self.make_content(label, id, score, confidence, topicality)
+        content = Content.find_or_create_by(description: label)
+        ArtworkContent.create(content_id: content.id, artwork_id: id, score: score, confidence: confidence, topicality: topicality )
     end
 
 
@@ -18,8 +16,7 @@ class Content < ApplicationRecord
         image_annotator = Google::Cloud::Vision::ImageAnnotator.new(credentials: JSON.parse(ENV.fetch("GOOGLE_APPLICATION_CREDENTIALS")))
         response = image_annotator.label_detection image: file
         response.responses.each do |res|
-            res.label_annotations.each { |label| content = Content.find_or_create_by(description:label.description.downcase)}    
-        ArtworkContent.create(content_id: content.id, artwork_id: id)
+            res.label_annotations.each { |label| Content.make_content(label.description.downcase, id, label.score, label.confidence, label.topicality)}
         end
     end
 
