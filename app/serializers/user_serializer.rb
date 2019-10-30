48a83ec36098ef6dc17502eec0a5516296ab7f7f
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-30 13:57:44 +0000

    removed password_digest from serialiser

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :password_digest, :biography, :public
+  attributes :id, :first_name, :last_name, :email, :biography, :public
 end
