01edee1c73e0ae93ae545aa116c893b884ad0d6f
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-30 17:52:01 +0000

    working for content only

--- app/models/content.rb
+++ app/models/content.rb
@@ -3,18 +3,27 @@ class Content < ApplicationRecord
     has_many :artwork_contents
     has_many :artworks, through: :artwork_contents
 
+
+    def self.make_content(data)
+   
+            
+            content = self.find_or_create_by(description: label.description.downcase)
+            ArtworkContent.create(content_id: content.id, artwork_id: id, score: label.score, confidence: label.confidence, topicality: label.topicality)
+    end
+
+
     require "google/cloud/vision"
 
     def self.google_cloud_vision(file, id)
         image_annotator = Google::Cloud::Vision::ImageAnnotator.new(credentials: JSON.parse(ENV.fetch("GOOGLE_APPLICATION_CREDENTIALS")))
         response = image_annotator.label_detection image: file
         response.responses.each do |res|
-            res.label_annotations.each do |label| 
-                content = self.find_or_create_by(description: label.description.downcase)
-                ArtworkContent.create(content_id: content.id, artwork_id: id, score: label.score, confidence: label.confidence, topicality: label.topicality)
-            end
+            res.label_annotations.each { |label| content = Content.find_or_create_by(description:label.description.downcase)}    
+        ArtworkContent.create(content_id: content.id, artwork_id: id)
         end
     end
 
+
+    
+
 end
-#ArtworkContent.create(content_object: label.description, artwork_id: id)
