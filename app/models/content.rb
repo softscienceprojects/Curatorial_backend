41df9f3120eeef17bb1a732ee5182a055dbfc91b
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-30 18:58:41 +0000

    removing whitespace and placing on one line seems to work

--- app/models/content.rb
+++ app/models/content.rb
@@ -4,20 +4,18 @@ class Content < ApplicationRecord
     has_many :artworks, through: :artwork_contents
 
 
-    def self.make_content(label, id, score, confidence, topicality)
+    def self.make_content(label, artworkid, score, confidence, topicality)
         content = Content.find_or_create_by(description: label)
-        ArtworkContent.create(content_id: content.id, artwork_id: id, score: score, confidence: confidence, topicality: topicality )
+        ArtworkContent.create(content_id: content.id, artwork_id: artworkid, score: score, confidence: confidence, topicality: topicality )
     end
 
 
     require "google/cloud/vision"
 
-    def self.google_cloud_vision(file, id)
+    def self.google_cloud_vision(file, artworkid)
         image_annotator = Google::Cloud::Vision::ImageAnnotator.new(credentials: JSON.parse(ENV.fetch("GOOGLE_APPLICATION_CREDENTIALS")))
         response = image_annotator.label_detection image: file
-        response.responses.each do |res|
-            res.label_annotations.each { |label| Content.make_content(label.description.downcase, id, label.score, label.confidence, label.topicality)}
-        end
+        response.responses.each{|res|res.label_annotations.each{|label|Content.make_content(label.description.downcase, artworkid, label.score, label.confidence, label.topicality)}}
     end
 
 
