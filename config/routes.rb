0cdd51dc251d708cb5023e7c4eb62a5a0f649ff8
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-31 14:55:32 +0000

    validate route

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   resources :exhibitions
   resources :users
   post '/signin', to: 'auth#create'
+  post '/validate', to: 'auth#validate'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
