e9a68a6fb4d2278c6ebdf22e4e01a2eee3539214
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-31 12:18:18 +0000

    create and strong params for login

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,2 +1,18 @@
 class AuthController < ApplicationController
+
+    def create
+        user = User.find_by(email: login_params[:email])
+        if user && user.authenticate(login_params[:password])
+            render json: {token: issue_token({user_id: user.id}), user: UserSerializer.new(user)}
+        else
+            render json: {errors: ["Login credentials are not valid"]}, status: :not_found
+        end
+    end
+
+    private
+
+    def login_params
+        params.require(:user).permit(:email, :password)
+    end
+
 end
