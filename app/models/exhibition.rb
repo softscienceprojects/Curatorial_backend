b8cffc4b3a9ed019e74d5e747cdeaa2d7d911240
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-01 11:15:09 +0000

    validations on exhibition

--- app/models/exhibition.rb
+++ app/models/exhibition.rb
@@ -2,12 +2,15 @@ class Exhibition < ApplicationRecord
   belongs_to :user
 
   has_many :artwork_exhibitions
-  has_many :artworks, through: artwork_exhibitions
+  has_many :artworks, through: :artwork_exhibitions
 
   has_many :exhibition_likes
 
   #has_many :reviews
 
   has_one_attached :photo
+  validates :user_id, presence: true
+  validates :summary, presence: true
+  
 
 end
