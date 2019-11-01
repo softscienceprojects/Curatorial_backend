4d5fbad0193aed2882e7d93831e1e65c52dce951
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-01 11:15:27 +0000

    starting to try artwork_serializer for nesting - does not work

--- app/serializers/artwork_serializer.rb
+++ app/serializers/artwork_serializer.rb
@@ -1,5 +1,13 @@
 class ArtworkSerializer < ActiveModel::Serializer
   attributes :id, :artist, :title, :medium, :description, :collection, :location, :image_url, :image_copyright, :permalink, :origin_id
-  has_many :artwork_contents
   has_many :contents, through: :artwork_contents
+
+  has_many :artworks_contents
+
+  def artworks_contents
+    object.artwork_contents.each do |artcontent|
+       artcontent
+    end
+  end
+
 end
