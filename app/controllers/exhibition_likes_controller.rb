96092a1315d715ca82333a78cda16958b5d2feee
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-03 16:12:11 +0000

    exhibition likes controller

--- app/controllers/exhibition_likes_controller.rb
+++ app/controllers/exhibition_likes_controller.rb
@@ -1,18 +1,19 @@
-class ExhibitionLikeController < ApplicationController
+class ExhibitionLikesController < ApplicationController
   def create
     exhibition_like = ExhibitionLike.create(exhibition_like_params)
     render json: exhibition_like
   end
 
   def destroy
-    exhibition_like = exhibition_like.find(params[:id])
+    exhibition_like = ExhibitionLike.find(params[:id])
     exhibition_like.destroy
+    render json: exhibition_like
   end
 
   private
 
   def exhibition_like_params
-    param.require(:exhibition_like).permit(:exhibition_id, :user_id)
+    params.require(:exhibition_like).permit(:exhibition_id, :user_id)
   end
-  
+
 end
