99a5039371c51d90fa6f06324c890f75813d9f75
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-03 21:22:44 +0000

    send user object back on update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,7 @@ class UsersController < ApplicationController
     def update
         user = User.find(params[:id])
         user.update(user_params)
+        render json: user
     end
 
     def destroy
