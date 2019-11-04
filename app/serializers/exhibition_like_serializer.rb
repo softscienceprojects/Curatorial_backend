4ade4f6d3f35091417b08d3ec19511463910967e
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-04 15:48:26 +0000

    exhibition like features exhibition

--- app/serializers/exhibition_like_serializer.rb
+++ app/serializers/exhibition_like_serializer.rb
@@ -1,11 +1,11 @@
 class ExhibitionLikeSerializer < ActiveModel::Serializer
-  attributes :id, :exhibition_id, :user_id #, :exhibitions
-  belongs_to :exhibition
+  attributes :id, :exhibition, :exhibition_id, :user_id #, :exhibitions
+  # include: [ :exhibition ]
 
-  def exhibitions
-    object.exhibitions.map do |exhibition|
-      ExhibitionSerializer.new(exhibition)
-    end
-  end
+  # def exhibitions
+  #   object.exhibitions.map do |exhibition|
+  #     ExhibitionSerializer.new(exhibition)
+  #   end
+  # end
 
 end
