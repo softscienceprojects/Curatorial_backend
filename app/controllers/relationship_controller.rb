eb1f1621442c69ac88f834df9876369590faad51
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-07 17:02:59 +0000

    prep for followers/following

--- app/controllers/relationship_controller.rb
+++ app/controllers/relationship_controller.rb
@@ -6,15 +6,18 @@ class RelationshipController < ApplicationController
 
     def create
         relationship = Relationship.create!(relationship_params)
+        render json: relationship
     end
 
     def destroy
-
+       relationship = relationship.find(params[:id])
+       relationship.destroy
+       render json: relationship
     end
 
     private
 
     def relationship_params
-        params.require(:relationship).permit(:follower)
+        params.require(:relationship).permit(:follower_id, :followed_id)
     end
 end
