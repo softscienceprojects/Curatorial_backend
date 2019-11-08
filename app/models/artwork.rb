b282fcd0f94b51de43fd545ee7e1b09ab26b7d0d
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-08 18:31:37 +0000

    ran further queries

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -8,7 +8,7 @@ class Artwork < ApplicationRecord
 
     def self.get_harvard_images(start_page, end_page)
         #pages 23361 / 10 records per query
-        ##LAST QUERY RAN: page 50, page < 60
+        ##LAST QUERY RAN: page 600, page < 700
         page = start_page
         while page < end_page
             response = RestClient.get "https://api.harvardartmuseums.org/object?apikey=#{ENV["HARVARD_KEY"]}&q=divison%3AModern%20and%20Contemporary%20Art&imagecount=1&page=#{page}"
@@ -33,7 +33,7 @@ class Artwork < ApplicationRecord
                     Content.google_cloud_vision(artwork.image_url, artwork.id)
                 end
             end
-        sleep 2
+        sleep 5
         page +=1
         end
     end
