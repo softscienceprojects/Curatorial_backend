541bb007fa4e76035031664381a814df60acf0b6
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-08 11:31:15 +0000

    include exhibitions for feed

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :biography, :public
+  attributes :id, :first_name, :last_name, :email, :biography, :public, :exhibitions
   has_many :exhibitions
 
   has_many :followed_users, through: :active_relationships, source: :followed_user
