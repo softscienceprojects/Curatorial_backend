8ebb32570362e56d6d43a1ba290842856e7352a3
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-09 15:35:32 +0000

    show active relationships for follow/unfollow

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :biography, :public, :exhibitions
+  attributes :id, :first_name, :last_name, :email, :biography, :public, :exhibitions, :active_relationships # :followed_users, :follower_users
   has_many :exhibitions
 
   has_many :followed_users, through: :active_relationships, source: :followed_user
