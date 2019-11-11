5b35031d41db8d1869a4e0698f25382b122ce2a9
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-11 09:31:58 +0000

    deploy v3

--- db/migrate/20191030134613_artwork_exhibition.rb
+++ db/migrate/20191030134613_artwork_exhibition.rb
@@ -1,4 +1,4 @@
-class ArtworkExhibition < ActiveRecord::Migration[6.0]
+class ArtworkExhibitions < ActiveRecord::Migration[6.0]
   def change
     create_table :artwork_exhibitions do |t|
       t.references :exhibition
