9fc03962ec4fa18443f776d3ea637340272b0d9c
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-11-12 11:29:49 +0000

    only send through artwork details and exhibitions

--- app/serializers/artwork_serializer.rb
+++ app/serializers/artwork_serializer.rb
@@ -1,16 +1,6 @@
 class ArtworkSerializer < ActiveModel::Serializer
-  attributes :id, :artist, :title, :medium, :description, :collection, :location, :image_url, :image_copyright, :permalink, :origin_id, :artwork_exhibitions, :contents
-  has_many :contents, through: :artwork_contents
+  attributes :id, :artist, :title, :medium, :description, :collection, :location, :image_url, :image_copyright, :permalink, :origin_id, :content_names
 
-  has_many :artwork_contents
-  has_many :contents, through: :artwork_contents
-  has_many :exhibitions, through: :artwork_exhibitions
-  has_many :artwork_exhibitions
-
-  def artworks_contents
-    object.artwork_contents.each do |artcontent|
-       artcontent
-    end
-  end
+  has_many :exhibitions
 
 end
